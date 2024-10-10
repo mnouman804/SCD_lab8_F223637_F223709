ddb0db764609223d757eff19e18c785ebcde68f4
UsmanAamir01 <f223709@cfd.nu.edu.pk>
2024-10-10 10:00:45 +0500

    Added addition and multiplication methods

--- src/pkg/Calculator.java
+++ src/pkg/Calculator.java
@@ -1,6 +1,14 @@
 package pkg;
 
 public class Calculator {
+	public int addition(int a, int b) {
+		return a + b;
+	}
+
+	public int multiplication(int a, int b) {
+		return a * b;
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
