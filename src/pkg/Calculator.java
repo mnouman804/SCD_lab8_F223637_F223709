c71aba39a72de748cd509ccda5a720acc04bf9b6
mnouman804 <f223637@cfd.nu.edu.pk>
2024-10-10 10:14:04 +0500

    Added Input Validation for addition and multiplication

--- src/pkg/Calculator.java
+++ src/pkg/Calculator.java
@@ -2,18 +2,27 @@ package pkg;
 
 public class Calculator {
 	public int addition(int a, int b) {
+		if (a < 0 || b < 0) {
+			throw new IllegalArgumentException("Inputs must be non-negative.");
+		}
 		return a + b;
 	}
 
 	public int multiplication(int a, int b) {
+		{
+	        if (a < 0 || b < 0) {
+	            throw new IllegalArgumentException("Inputs must be non-negative.");
+	        }
 		return a * b;
+		}
 	}
-
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Calculator obj = new Calculator();
 		System.out.println("System");
-
+		System.out.println(obj.addition(1,2));
+		System.out.println(obj.multiplication(1,2));
 	}
+	
 
 }
