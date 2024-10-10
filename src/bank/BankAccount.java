4323441261dc21c3143aff51cd89433afc6b0aea
mnouman804 <f223637@cfd.nu.edu.pk>
2024-10-10 11:39:54 +0500

    Added Deposit Method

--- src/bank/BankAccount.java
+++ src/bank/BankAccount.java
@@ -10,4 +10,16 @@ public class BankAccount {
             return false; 
         }
     }
+    public boolean deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            return true;
+        } else {
+            return false;  
+        }
+    }
+    public static void main(String [] args)
+    {
+    	
+    }
 }
